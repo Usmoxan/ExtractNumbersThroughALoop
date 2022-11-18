23f5ac5f5036cf508d3b0cb3457b25669572c3d9
Usmoxan <101461756+Usmoxan@users.noreply.github.com>
2022-11-18 14:18:45 +0500

    main.dart
    Extract numbers through a loop with dart

---
+++ main.dart
@@ -0,0 +1,10 @@
+//Extract numbers through a loop
+void main() {
+  var a = 726363;
+  String string_number = a.toString();
+
+  for (int i = 0; i < string_number.length; i++) {
+    print(string_number[i]);
+  }
+}
+//usmoxandev
